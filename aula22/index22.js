892d9eea5ee63d33aa6e9055ecd4c618eafe40b8
Ana Flavia <anaflavia.silvino18@gmail.com>
2024-12-30 10:21:24 -0300

    atualizar

--- aula22/index22.js
+++ aula22/index22.js
@@ -5,3 +5,13 @@ Operadores lógicos
 ! -> NOT -> NÃO
 */
 
+// console.log(true && true && true); // && - Todas as expressões precisam ser verdadeiras para retornar true.
+// const expressao = true && false && true && true;
+// console.log(expressao);
+
+// console.log(true || true || true); // || - Apenas uma expressão precisa ser verdadeira paa retornar true, porém se todas forem falsas, retornará false.
+// const expressao = false || false || true || false;
+// console.log(expressao);
+
+ // console.log(!false); // ! - A expressão é uma negação e retorna o valor inverso da variável, se colocamos true ela retornará false. Se false, true!
+ // !! = RETORNA PRO VALOR ORIGINAL!
