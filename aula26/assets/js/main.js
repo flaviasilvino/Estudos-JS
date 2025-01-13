5fe5f9f5a385a100c6639d0f96e42d2e2cab7873
Ana Flavia <anaflavia.silvino18@gmail.com>
2025-01-13 13:33:10 -0300

    atualização1301

--- aula26/assets/js/main.js
+++ aula26/assets/js/main.js
@@ -4,11 +4,59 @@ const form = document.querySelector('#formulario');
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log('Evento previnido.');
-    setResultado('Olá!');
-});
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
+    console.log(inputPeso);
+    console.log(inputAltura);
 
-function setResultado (msg) {
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if(!peso){
+        setResultado('Peso inválido', false);
+        return;
+    }
+
+    if(!altura){
+        setResultado('Altura inválido', false);
+        return;
+    }
+
+    const imc = getImc(peso, altura);
+    const NivelImc = getNivelImc(imc);
+
+    const msg = `Seu IMC é ${imc} (${NivelImc}).`;
+
+    setResultado(msg, true);
+});
+function getNivelImc (imc) {
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso',
+        'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+
+    if (imc >= 39.9) return nivel[5];
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];
+    if (imc < 18.5) return nivel[0];
+
+}
+
+function getImc (peso, altura) {
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2); 
+}
+
+function criaP () {
+    const p = document.createElement('p');
+    p.innerHTML = msg;
+    resultado.appendChild(p);
+}
+
+function setResultado (msg, isValid) {
     const resultado = document.querySelector('#resultado');
-    resultado.innerHTML = `<p>${msg}</p>`;
+    resultado.innerHTML = '';
+
+    const p = criaP();
+
 }
