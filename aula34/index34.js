e7b1714030263f8b624fe4c4f4f9a833f20e0844
Ana Flavia <anaflavia.silvino18@gmail.com>
2025-01-17 12:37:42 -0300

    atualização1701

---
+++ aula34/index34.js
@@ -0,0 +1,18 @@
+//         (repetição)
+/*for (let i = 0; i <= 10; i++) { // i += 10 (de 10 em 10) | i-- (decremento)
+    console.log(`Linha ${i}`);
+}*/
+
+//         (par ou impar)
+/*for (let i = 0; i <= 10; i++) { 
+    const par = i % 2 ==0 ? 'par' : 'ímpar';
+    console.log(i, par);
+}
+*/
+//                0       1       2
+const frutas = ['Maçã', 'Pêra', 'Uva'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+}
+
