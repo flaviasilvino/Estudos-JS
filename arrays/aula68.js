1b7b8356afe1c2bc0c7dbaed22bc2ee1885fde1a
Ana Flavia <anaflavia.silvino18@gmail.com>
2025-02-04 13:42:19 -0300

    atualização0402

---
+++ arrays/aula68.js
@@ -0,0 +1,11 @@
+// Retorne a soma do dobro de todos os pares
+// Filtrar pares
+// Dobrar os valores
+// Reduzir (somar tudo)
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+const numerosPares = numeros.filter(function(valor) {
+    return valor % 2 === 0;
+}).map();
+
+console.log(numerosPares);
