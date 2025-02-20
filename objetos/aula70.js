b68c1be69c6ba9a92e6a57df1c9912ab3a873ce7
flaviasilvino <flaviasilvino@vintglobal.com.br>
2025-02-20 13:58:14 -0300

    atualização2002

--- objetos/aula70.js
+++ objetos/aula70.js
@@ -1,6 +1,31 @@
-const pessoa = {
-    nome: 'Luiz',
-    sobrenome: 'Otávio'
+/*const pessoa1 = new Object();
+pessoa1.nome = 'Ana';
+pessoa1.sobrenome = 'Oliveira';
+pessoa1.idade = 22;
+pessoa1.falarNome = function() {
+    return (`${this.nome} está falando seu nome.`);
 };
-console.log(pessoa['nome']);
-console.log(pessoa['sobrenome']);
+
+pessoa1.getDataNascimento = function() {
+    const dataAtual = new Date();
+    return dataAtual.getFullYear() - this.idade;
+};
+
+for (let chave in pessoa1) {
+    console.log(pessoa1[chave]);
+}
+*/
+
+// Factory functions / Constructor functions / Classes
+function criaPessoa(nome, sobrenome) {
+    return {
+        nome,
+        sobrenome,
+        get nomeCompleto(){
+            return `${this.nome} ${this.sobrenome}`;
+        }
+    };
+}
+
+const p1 = criaPessoa('Ana', 'Flávia');
+console.log(p1.nomeCompleto());
---
+++ objetos/aula77.js
@@ -0,0 +1,7 @@
+function ValidaCPF(cpfEnviado){
+    Object.defineProperty(this, 'cpfLimpo', {
+
+    });
+}
+const cpf = new ValidaCPF('628.757.803-36');
+cpf.valida();
